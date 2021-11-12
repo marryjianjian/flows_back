cddda68046a87828ab5b0da9da671abedafe5159
marryjianjian <logjian@gmail.com>
2021-11-13 01:52:02 +0800

    chore: use AUTOINCREMENT for PRIMARY KEY

--- src/db/init.sql
+++ src/db/init.sql
@@ -1,11 +1,10 @@
 CREATE TABLE access_info (
-    id INT,
+    id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
     time TEXT,
     src_port INT,
     src_ip TEXT,
     dst_port INT,
     dst_domain TEXT,
     state TEXT,
-    protocol TEXT,
-    PRIMARY KEY(id ASC)
+    protocol TEXT
 );
